dc9a4bbc36a7e9a969b261fafe39480de1e98589
Mark Eaton <MarkEEaton@users.noreply.github.com>
2021-06-24 22:42:40 -0400

    create results dict

--- parse.py
+++ parse.py
@@ -1,4 +1,4 @@
-from collections import Counter
+from collections import Counter, defaultdict
 from nltk.corpus import stopwords
 import langdetect
 import nltk
@@ -6,6 +6,7 @@ import string
 
 def novel():
     with open("test.txt", "r", encoding="latin-1") as f1:
+        results = defaultdict(list)
         novel_words = 0
         sentences = nltk.tokenize.sent_tokenize(f1.read())
         for sentence in sentences:
@@ -31,22 +32,23 @@ def novel():
                 words = [word for word in words if word not in stopwords.words("english")]
                 first_letters = [word[0] for word in words]
                 c = Counter()
-                for letter in "a":
-                    for l in first_letters:
-                        if l == letter:
-                            c[l] += 1
-                    try:
-                        if novel_words > 50:
-                            return
-                        if c.most_common(1)[0][1] / len(words) > 0.6:
-                            with open("novel.txt", "a", encoding="latin-1") as f2:
-                                novel_words += sentence_words
-                                print(sentence)
-                                f2.write(sentence + " ")
-                    except IndexError:
-                        pass
-                    except:
-                        raise
+                for l in first_letters:
+                    c[l] += 1
+                try:
+                    selected_letter = c.most_common(1)[0][0]
+                    if novel_words > 50:
+                        print(results)
+                        return
+                    if c.most_common(1)[0][1] / len(words) > 0.6:
+                        results[selected_letter].append(sentence)
+                        with open("novel.txt", "a", encoding="latin-1") as f2:
+                            novel_words += sentence_words
+                            print(sentence, selected_letter)
+                            f2.write(sentence + " ")
+                except IndexError:
+                    pass
+                except:
+                    raise
 
 if __name__ == "__main__":
     novel()
