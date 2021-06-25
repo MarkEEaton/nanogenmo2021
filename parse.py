0bebc6dce66dabe4b12fcba37821c0d890e183bf
Mark Eaton <MarkEEaton@users.noreply.github.com>
2021-06-24 22:51:08 -0400

    break out assemble function; blacken; add nato

--- parse.py
+++ parse.py
@@ -4,6 +4,36 @@ import langdetect
 import nltk
 import string
 
+nato = [
+    "Alfa",
+    "Bravo",
+    "Charlie",
+    "Delta",
+    "Echo",
+    "Foxtrot",
+    "Golf",
+    "Hotel",
+    "India",
+    "Juliett",
+    "Kilo",
+    "Lima",
+    "Mike",
+    "November",
+    "Oscar",
+    "Papa",
+    "Quebec",
+    "Romeo",
+    "Sierra",
+    "Tango",
+    "Uniform",
+    "Victor",
+    "Whiskey",
+    "Xray",
+    "Yankee",
+    "Zulu",
+]
+
+
 def novel():
     with open("test.txt", "r", encoding="latin-1") as f1:
         results = defaultdict(list)
@@ -15,7 +45,7 @@ def novel():
                 language = langdetect.detect(sentence)
             except langdetect.lang_detect_exception.LangDetectException:
                 language = "none"
-            sentence = sentence.replace('"', '')
+            sentence = sentence.replace('"', "")
             sentence = sentence.replace("(", "")
             sentence = sentence.replace(")", "")
             sentence = sentence.replace("_", "")
@@ -23,13 +53,15 @@ def novel():
             sentence = sentence.replace("]", "")
             sentence = sentence.replace("*", "")
             sentence = " ".join(sentence.split())
-            if sentence.isupper() or (language != 'en'):
+            if sentence.isupper() or (language != "en"):
                 pass
             else:
                 words = nltk.tokenize.word_tokenize(sentence)
                 words = [word.lower() for word in words if word.isalpha()]
                 sentence_words = len(words)
-                words = [word for word in words if word not in stopwords.words("english")]
+                words = [
+                    word for word in words if word not in stopwords.words("english")
+                ]
                 first_letters = [word[0] for word in words]
                 c = Counter()
                 for l in first_letters:
@@ -38,17 +70,22 @@ def novel():
                     selected_letter = c.most_common(1)[0][0]
                     if novel_words > 50:
                         print(results)
-                        return
+                        return results
                     if c.most_common(1)[0][1] / len(words) > 0.6:
                         results[selected_letter].append(sentence)
-                        with open("novel.txt", "a", encoding="latin-1") as f2:
-                            novel_words += sentence_words
-                            print(sentence, selected_letter)
-                            f2.write(sentence + " ")
+                        novel_words += sentence_words
+                        print(sentence, selected_letter)
                 except IndexError:
                     pass
                 except:
                     raise
 
+
+def assemble(nov):
+    with open("novel.txt", "a", encoding="latin-1") as f2:
+        pass
+
+
 if __name__ == "__main__":
-    novel()
+    nov = novel()
+    assemble(nov)
